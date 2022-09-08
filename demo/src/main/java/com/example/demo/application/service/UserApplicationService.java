06190bd99df507584be3c3b4614024467b7d6232
Dirk Hiltmann <Dirk@hiltisoft.de>
2022-09-08 18:35:49 +0200

    6.2.2 Edit Error messages

--- demo/src/main/java/com/example/demo/application/service/UserApplicationService.java
+++ demo/src/main/java/com/example/demo/application/service/UserApplicationService.java
@@ -1,4 +1,4 @@
-package com.example.demo.controller.service;
+package com.example.demo.application.service;
 
 import java.util.LinkedHashMap;
 import java.util.Locale;
--- demo/src/main/java/com/example/demo/controller/service/SignUpController.java
+++ demo/src/main/java/com/example/demo/controller/service/SignUpController.java
@@ -5,12 +5,13 @@ import java.util.Locale;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.example.demo.controller.service.UserApplicationService;
+import com.example.demo.application.service.UserApplicationService;
 import com.example.demo.form.SignupForm;
 
 import lombok.extern.slf4j.Slf4j;
@@ -35,7 +36,14 @@ public class SignUpController {
 	}
 
 	@PostMapping("/signup")
-	public String postSignup(SignupForm form ) {
+	public String postSignup(Model model, Locale locale,
+		@ModelAttribute SignupForm form, BindingResult bindingResult) {
+		
+		//Input check result
+		if (bindingResult.hasErrors()) {
+			//NG: Return to the user signup screen
+			return getSignup(model, locale, form);
+		}
 		
 		log.info(form.toString());
 		
