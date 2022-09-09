4e330337989ea271abcefc68cf765bfeac7d32d0
Dirk Hiltmann <Dirk@hiltisoft.de>
2022-09-09 17:51:55 +0200

    Kapitel 6.3 Validation

--- demo/src/main/java/com/example/demo/controller/service/SignUpController.java
+++ demo/src/main/java/com/example/demo/controller/service/SignUpController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,7 +38,7 @@ public class SignUpController {
 
 	@PostMapping("/signup")
 	public String postSignup(Model model, Locale locale,
-		@ModelAttribute SignupForm form, BindingResult bindingResult) {
+		@ModelAttribute @Validated SignupForm form, BindingResult bindingResult) {
 		
 		//Input check result
 		if (bindingResult.hasErrors()) {
--- demo/src/main/java/com/example/demo/form/SignupForm.java
+++ demo/src/main/java/com/example/demo/form/SignupForm.java
@@ -2,16 +2,37 @@ package com.example.demo.form;
 
 import java.sql.Date;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
 import lombok.Data;
 
 @Data
 public class SignupForm {
+	
+	@NotBlank
+	@Email
 	private String userId;
+	
+	@NotBlank
+	@Length(min=4,max=100)
 	private String password;
+	
+	@NotBlank
 	private String userName;
+	
 	@DateTimeFormat(pattern="dd/MM/yyyy")
 	private String birthday;
+	
+	@Min(20)
+	@Max(100)
 	private Integer age;
+	
+	@NotNull
 	private Integer gender;	
 }
