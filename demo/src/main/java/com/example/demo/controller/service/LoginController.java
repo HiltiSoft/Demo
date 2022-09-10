5bc2f32412075084da45ef4cd68b1937bf9f9162
Dirk Hiltmann <Dirk@hiltisoft.de>
2022-09-10 18:41:08 +0200

    7. Screen Layout

--- demo/src/main/java/com/example/demo/controller/service/LoginController.java
+++ demo/src/main/java/com/example/demo/controller/service/LoginController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller.service;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
@@ -10,6 +11,12 @@ public class LoginController {
 	/** Display login screen */
 	public String getLogin() {
 		return "login/login";
+	}
+	
+	/** Redirect to user list screen */
+	@PostMapping("login")
+	public String postLogin() {
+		return "redirect:/user/list";
 	}
 	
 }
